9b6a416f8e00e8119caf21966b470165cd36888c
Cristian Guzman <cristiangt1813@gmail.com>
2024-12-14 21:08:35 -0500

    modify homepage

--- pomodoro/src/pages/home/HomePage.tsx
+++ pomodoro/src/pages/home/HomePage.tsx
@@ -3,7 +3,7 @@ import PomodoroTimer from '@components/PomodoroTimer'
 export const HomePage = () => {
   return (
         <>
-            <div>HomePage</div>
+            <div>Home Page</div>
             <PomodoroTimer/>
         </>
   )
